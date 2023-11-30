e505e0449ecbe7d126eb99fafce770b4b6091add
Soumencr7 <sagar.malikcr7@gmail.com>
2023-11-30 22:04:36 +0530

    newly
    gghfhrf

---
+++ java/constructor/test1.java
@@ -0,0 +1,15 @@
+//no_argument constructor(user defined).
+package constructor;
+
+public class test1 {
+
+    test1() {
+        System.out.println("no argument constructor");
+    }
+
+    public static void main(String[] args) {
+        test1 t = new test1();
+
+    }
+
+}
---
+++ java/constructor/test2.java
@@ -0,0 +1,14 @@
+//parameterize constructor;
+package constructor;
+
+public class test2 {
+    test2(String name) {
+        System.out.println("parametarized constructor");
+    }
+
+    public static void main(String[] args) {
+        test2 t = new test2("sagar");
+
+    }
+
+}
